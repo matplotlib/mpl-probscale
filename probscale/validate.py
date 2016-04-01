ac8ed0712e4862d4de6a39a0b6e726b2c9e4d280
Paul Hobson <pmhobson@gmail.com>
2016-04-01 15:35:58 -0700

    add validation submodule

---
+++ probscale/validate.py
@@ -0,0 +1,44 @@
+from matplotlib import pyplot
+
+
+def axes_object(ax):
+    """ Checks if a value if an Axes. If None, a new one is created.
+
+    """
+
+    if ax is None:
+        fig, ax = pyplot.subplots()
+    elif isinstance(ax, pyplot.Axes):
+        fig = ax.figure
+    else:
+        msg = "`ax` must be a matplotlib Axes instance or None"
+        raise ValueError(msg)
+
+    return fig, ax
+
+
+def axis_name(axname, argname):
+    valid_args = ['x', 'y']
+    if axname.lower() not in valid_args:
+        msg = 'Invalid value for {} ({}). Must be on of {}.'
+        raise ValueError(msg.format(argname, axname, valid_args))
+
+    return axname.lower()
+
+
+def fit_argument(arg, argname):
+    valid_args = ['x', 'y', 'both', None]
+    if arg not in valid_args:
+        msg = 'Invalid value for {} ({}). Must be on of {}.'
+        raise ValueError(msg.format(argname, arg, valid_args))
+
+    return arg
+
+
+def axis_type(axtype):
+    if axtype.lower() not in ['pp', 'qq', 'prob']:
+        raise ValueError("invalid axtype: {}".format(axtype))
+    return axtype.lower()
+
+
+
